d7f6a7e32e9cf68645dca6767f34bcc6dc1ee218
Lu Wang <coolwanglu@gmail.com>
2014-07-24 22:18:50 -0700

    working

--- build.sh
+++ build.sh
@@ -3,24 +3,33 @@ set -e
 
 EM_DIR=~/src/emscripten
 BASE_DIR=`pwd`
-ZLIB_DIR=$EM_DIR/tests/zlib
-FREETYPE_DIR=$EM_DIR/tests/freetype
+#FREETYPE_DIR=$EM_DIR/tests/freetype
+FREETYPE_DIR=$BASE_DIR/freetype-2.5.3
 POPPLER_DIR=$BASE_DIR/poppler-0.26.3
-
-do_zlib() {
-pushd $ZLIB_DIR
-$EM_DIR/emconfigure ./configure \
-
-$EM_DIR/emmake make
-popd
-}
 
 do_freetype () {
 pushd $FREETYPE_DIR
+CPPFLAGS="-Oz" \
 $EM_DIR/emconfigure ./configure \
     --enable-shared=no \
+    --enable-static=yes \
+    --disable-biarch-config \
+    --without-zlib \
+    --without-bzip2 \
+    --without-png \
+    --without-harfbuzz \
+    --without-old-mac-fonts \
+    --without-fsspec \
+    --without-fsref \
+    --without-quickdraw-toolbox \
+    --without-quickdraw-carbon \
+    --without-ats \
 
-$EM_DIR/emmake make
+make -j8
+#ugly hack
+gcc -o objs/apinames src/tools/apinames.c
+make -j8
+
 popd
 }
 
@@ -30,6 +39,7 @@ FONTCONFIG_CFLAGS="-I$BASE_DIR" \
 FONTCONFIG_LIBS=' ' \
 FREETYPE_CFLAGS="-I$EM_DIR/tests/freetype/include" \
 FREETYPE_LIBS=' ' \
+CPPFLAGS="-Oz" \
 $EM_DIR/emconfigure ./configure \
     --enable-shared=no \
     --disable-xpdf-headers \
@@ -38,7 +48,7 @@ $EM_DIR/emconfigure ./configure \
     --disable-libtiff \
     --disable-largefile \
     --disable-zlib \
-    --disable-libcurl
+    --disable-libcurl \
     --disable-libjpeg \
     --disable-libpng \
     --enable-splash-output \
@@ -55,7 +65,7 @@ $EM_DIR/emconfigure ./configure \
     --enable-cms=none \
     --without-x \
 
-$EM_DIR/emmake make
+make -j8
 popd
 }
 
@@ -63,18 +73,15 @@ popd
 do_link () {
 pushd web
 $EM_DIR/emcc \
-    -O2 \
-    $POPPLER_DIR/utils/pdftoppm.o \
+    -Oz \
+    --llvm-opt 1 \
     $POPPLER_DIR/poppler/.libs/libpoppler.a \
-    $POPPLER_DIR/utils/parseargs.o \
-    $ZLIB_DIR/libz.a \
     $FREETYPE_DIR/objs/.libs/libfreetype.a \
     -o poppler.js 
 
 popd
 }
 
-#do_zlib
-#do_freetype
-#do_poppler
-do_link
+do_freetype
+do_poppler
+#do_link
