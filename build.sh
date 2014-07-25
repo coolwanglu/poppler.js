2d99d640d2a4c9c0b384d4ef8034ba9a48c49d90
Lu Wang <coolwanglu@gmail.com>
2014-07-24 21:47:21 -0700

    update poppler

--- build.sh
+++ build.sh
@@ -4,23 +4,12 @@ set -e
 EM_DIR=~/src/emscripten
 BASE_DIR=`pwd`
 ZLIB_DIR=$EM_DIR/tests/zlib
-LIBPNG_DIR=$BASE_DIR/libpng-1.6.7
 FREETYPE_DIR=$EM_DIR/tests/freetype
-POPPLER_DIR=$BASE_DIR/poppler-0.24.4
+POPPLER_DIR=$BASE_DIR/poppler-0.26.3
 
 do_zlib() {
 pushd $ZLIB_DIR
 $EM_DIR/emconfigure ./configure \
-
-$EM_DIR/emmake make
-popd
-}
-
-do_libpng () {
-pushd $LIBPNG_DIR
-CPPFLAGS=-I$ZLIB_DIR \
-$EM_DIR/emconfigure ./configure \
-    --enable-shared=no \
 
 $EM_DIR/emmake make
 popd
@@ -37,27 +26,32 @@ popd
 
 do_poppler () {
 pushd $POPPLER_DIR
-LIBPNG_CFLAGS="-I$BASE_DIR/libpng-1.6.7" \
-LIBPNG_LIBS=' ' \
 FONTCONFIG_CFLAGS="-I$BASE_DIR" \
 FONTCONFIG_LIBS=' ' \
 FREETYPE_CFLAGS="-I$EM_DIR/tests/freetype/include" \
 FREETYPE_LIBS=' ' \
 $EM_DIR/emconfigure ./configure \
     --enable-shared=no \
+    --disable-xpdf-headers \
+    --enable-single-precision \
     --disable-libopenjpeg \
     --disable-libtiff \
     --disable-largefile \
+    --disable-zlib \
+    --disable-libcurl
     --disable-libjpeg \
-    --enable-libpng \
+    --disable-libpng \
+    --enable-splash-output \
     --disable-cairo-output \
     --disable-poppler-glib \
+    --enable-introspection=no \
     --disable-gtk-doc \
     --disable-gtk-doc-html \
     --disable-poppler-qt4 \
     --disable-poppler-qt5 \
     --disable-poppler-cpp \
     --disable-gtk-test \
+    --enable-utils \
     --enable-cms=none \
     --without-x \
 
@@ -73,7 +67,6 @@ $EM_DIR/emcc \
     $POPPLER_DIR/utils/pdftoppm.o \
     $POPPLER_DIR/poppler/.libs/libpoppler.a \
     $POPPLER_DIR/utils/parseargs.o \
-    $LIBPNG_DIR/.libs/libpng16.a \
     $ZLIB_DIR/libz.a \
     $FREETYPE_DIR/objs/.libs/libfreetype.a \
     -o poppler.js 
@@ -82,7 +75,6 @@ popd
 }
 
 #do_zlib
-#do_libpng
 #do_freetype
 #do_poppler
 do_link
